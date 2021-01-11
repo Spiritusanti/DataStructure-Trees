a8d6c09fa2ca6c563593b743ab28c64bbd44b885
Jacob McCracken <mccrackenjacob@gmail.com>
2021-01-11 18:13:21 +0000

    implemented binary search tree - ugh......

--- index.js
+++ index.js
@@ -28,3 +28,132 @@ function BinaryTree(value) {
 
 // pros of BSTs - better than O(n), ordered, flexible size
 // cons of BSTs - no O(1) operations
+
+class Node {
+  constructor(value){
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+
+class BinarySearchTree {
+  constructor(){
+    this.root = null;
+  }
+  insert(value) {
+    // add a new value to the tree
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while(true) {
+        if(value < currentNode.value) {
+          // left
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left
+        } else {
+          // right
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
+
+  }
+  lookup(value) {
+    if (!this.root) {
+      return false
+    } 
+    let currentNode = this.root;
+    while(currentNode) {
+      // traverse left
+      if(value < currentNode.value) {
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        // traverse right
+        currentNode = currentNode.right;
+      } else if (currentNode.value === value) {
+        return currentNode
+      }
+    }
+    return false;
+  }
+  remove(value) {
+    if(!this.root) {
+      return false;
+    }
+    let currentNode = this.root;
+    let parentNode = null;
+    while(currentNode) {
+      if(value < currentNode.value) {
+        parentNode = currentNode;
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        parentNode = currentNode;
+        currentNode = currentNode.right;
+      } else if (currentNode.value === value) {
+        // option 1 - no right child
+        if(currentNode.right === null) {
+          parentNode.left = currentNode.left;
+          // option 2 right child doesn't have a left child
+        } else if (currentNode.right.left === null){
+          if(parentNode === null) {
+            this.root = currentNode.left;
+          } else {       
+            currentNode.right.left = currentNode.left;
+          // if parent > current, make right child left of the parent
+          if(currentNode.value < parentNode.value) {
+            parentNode.left = currentNode.right;
+          } else if (currentNode.value > parentNode.value) {
+            parentNode.right = currentNode.right;
+          }
+          }
+          // option 3 right child has a left child
+        } else {
+          // find the right child's left most child
+          let leftMost = currentNode.right.left;
+          let leftMostParent = currentNode.right;
+          while(leftMost.left !== null) {
+            leftMostParent = leftMost;
+            leftMost = leftMost.left;
+          }
+
+          // parent's left subtree is now leftmost's right subtree
+          leftMostParent.left = leftMost.right;
+          leftmost.left = currentNode.left;
+          leftmost.right = currentNode.right;
+
+          if(parentNode === null) {
+            this.root = leftMost;
+          } else {
+            if(currentNode.value < parentNode.value) {
+              parentNode.left = leftMost;
+            } else if (currentNode.value > parentNode.value) {
+              parentNode.right = leftMost;
+            }
+          }
+        }
+        return true;
+      }
+    }
+  }
+}
+
+const tree = new BinarySearchTree();
+
+function traverse(node) {
+  const tree = {value: node.value};
+  tree.left = node.left === null ? null :
+  traverse(node.left);
+  tree.right = node.right === null ? null :
+  traverse(node.right);
+  return tree;
+}
